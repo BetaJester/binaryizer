217ae00c4f1d516dd995a5aab7446cde8095922b
Beta Jester <betajester@betajester.com>
2021-03-11 14:23:56 +1100

    README updated, examples tested

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Simple header only C++ serialization library
 
 Very simple, your class or struct needs one or two functions/methods, `binaryize` and `debinaryize`. 
 
-If they are class methods, the signature is as such:
+**As class methods**
 
 ```cpp
 struct thing {
@@ -35,7 +35,7 @@ struct thing {
 };
 ```
 
-If they are standalone functions the signature is such:
+**As standalone functions**
 
 ```cpp
 struct thing {
@@ -53,7 +53,53 @@ void debinaryize(Archive &archive, thing &e) {
 }
 ```
 
-Note the intentional use of templates that have literally no indication that this library is being used, or any interest in what `Archive` is, simply that it is callable with a variable number of argument. This allows classes to be created without even including the library headers, zero dependencies.
+Note the use of templates with no indication that this library is being used, or any interest in what `Archive` is, simply that it is callable with a variable number of arguments. This allows classes to be created without even including the library headers, zero dependencies.
 
-If you are using standalone functions, it is best to place them in the same namespace as your class (it will work due to [Argument Dependent Lookup](https://en.cppreference.com/w/cpp/language/adl)), in the global namespace, or in the `bj` namespace if you don't want to pollute the global namespace.
+If you are using standalone functions, it is best to place them in the same namespace as your class ([ADL](https://en.cppreference.com/w/cpp/language/adl) will find it), in the global namespace, or in the `bj` namespace if you don't want to pollute the global namespace.
 
+## Versioning your binaries
+
+It's not a bad idea to spend a few bytes to version your classes, allowing for future changes. Here, assume versioning was always used (it's quite hard to add it later) and that version 1 had `a`, `b` and `c` members and version 2 added a `d` member.
+
+```cpp
+struct thing {
+    int a, b, c;
+    int d;
+
+    template<typename Archive>
+    void binaryize(Archive &archive) const {
+        archive(2, a, b, c, d);
+    }
+
+    template<typename Archive>
+    void debinaryize(Archive &archive) {
+        const int version = archive.get<int>();
+        switch (version) {
+        case 1: 
+            archive(a, b, c);
+            d = 5;
+            break;
+        case 2:
+            archive(a, b, c, d);
+            break;
+        default:
+            throw std::runtime_error("Unknow version for 'thing' in binary");
+        }
+    }
+};
+```
+
+As long as newly added members have a sensible default (or calculable) value, expansion of classes is pretty painless. This allows you to load older data, and the next time it is saved the new data will be version 2.
+
+You may note the use of `archive.get<int>();`{:.language-cpp} in the above. There are a number of methods supported, they are listed in *coming soon, sorry*. It would have been easy to use `archive(version)` but const correctness can be important.
+
+
+
+## TODO
+
+- [ ] STL header & tests
+- [ ] Endian optional
+- [ ] Unique pointers
+- [ ] Inheritance docs and tests
+- [ ] Document all methods
+- [ ] Expose all methods?
---
+++ tests/readme_tests.cpp
@@ -0,0 +1,150 @@
+// Copyright (C) 2021 Glenn Duncan <betajester@betajester.com>
+// See README.md, LICENSE, or go to https://github.com/BetaJester/binaryizer
+// for details.
+
+// Prove that *all* code in README is correct.
+
+#include <sstream>
+#include <catch2/catch.hpp>
+#include <bj/iostream_binaryizer.hpp>
+
+namespace readme_internal {
+    struct thing {
+        int a, b, c;
+
+        template<typename Archive>
+        void binaryize(Archive &archive) const {
+            archive(a, b, c);
+        }
+
+        template<typename Archive>
+        void debinaryize(Archive &archive) {
+            archive(a, b, c);
+        }
+
+        // Not in readme.
+        auto operator<=>(const thing &) const = default;
+    };
+} // namespace readme_internal.
+
+namespace readme_external {
+    struct thing {
+        int a, b, c;
+    
+        // Not in readme.
+        auto operator<=>(const thing &) const = default;
+    };
+
+    template<typename Archive>
+    void binaryize(Archive &archive, const thing &e) {
+        archive(e.a, e.b, e.c);
+    }
+
+    template<typename Archive>
+    void debinaryize(Archive &archive, thing &e) {
+        archive(e.a, e.b, e.c);
+    }
+
+} // namespace readme_external.
+
+namespace readme_versioning {
+
+    struct thing {
+        int a, b, c;
+        int d;
+
+        template<typename Archive>
+        void binaryize(Archive &archive) const {
+            archive(2, a, b, c, d);
+        }
+
+        template<typename Archive>
+        void debinaryize(Archive &archive) {
+            const int version = archive.get<int>();
+            switch (version) {
+            case 1: 
+                archive(a, b, c);
+                d = 5;
+                break;
+            case 2:
+                archive(a, b, c, d);
+                break;
+            default:
+                throw std::runtime_error("Unknow version for 'thing' in binary");
+            }
+        }
+
+        // Not in readme.
+        auto operator<=>(const thing &) const = default;
+    };
+
+} // namespace readme_versioning.
+
+// Save a couple of runtime tests.
+static_assert(bj::is_binaryizable_v<readme_internal::thing>);
+static_assert(bj::is_binaryizable_v<readme_external::thing>);
+static_assert(bj::is_binaryizable_v<readme_versioning::thing>);
+
+TEST_CASE("readme internal methods", "[readme,internal]") {
+
+    readme_internal::thing thing1{ .a = 1, .b = 3, .c = 5 };
+    readme_internal::thing thing2{ .a = 2, .b = 4, .c = 6 };
+
+    bj::iostream_binaryizer<std::stringstream> iobin(std::ios::binary | std::ios::in | std::ios::out);
+    bj::ibinaryizer &ibin = iobin;
+    bj::obinaryizer &obin = iobin;
+
+    REQUIRE(thing1 != thing2);
+    obin(thing1);
+    ibin(thing2);
+    REQUIRE(thing1 == thing2);
+}
+
+TEST_CASE("readme external methods", "[readme,external]") {
+
+    readme_external::thing thing1{ .a = 1, .b = 3, .c = 5 };
+    readme_external::thing thing2{ .a = 2, .b = 4, .c = 6 };
+
+    bj::iostream_binaryizer<std::stringstream> iobin(std::ios::binary | std::ios::in | std::ios::out);
+    bj::ibinaryizer &ibin = iobin;
+    bj::obinaryizer &obin = iobin;
+
+    REQUIRE(thing1 != thing2);
+    obin(thing1);
+    ibin(thing2);
+    REQUIRE(thing1 == thing2);
+}
+
+TEST_CASE("readme versioning methods", "[readme,internal,versioning]") {
+
+    readme_versioning::thing thing1{ .a = 1, .b = 3, .c = 5, .d = 7 };
+    readme_versioning::thing thing2{ .a = 2, .b = 4, .c = 6, .d = 8 };
+
+    bj::iostream_binaryizer<std::stringstream> iobin(std::ios::binary | std::ios::in | std::ios::out);
+    bj::ibinaryizer &ibin = iobin;
+    bj::obinaryizer &obin = iobin;
+
+    REQUIRE(thing1 != thing2);
+    obin(thing1);
+    ibin(thing2);
+    REQUIRE(thing1 == thing2);
+}
+
+TEST_CASE("readme versioning correctness", "[readme,internal,versioning]") {
+
+    readme_versioning::thing thing1{ .a = 1, .b = 3, .c = 5, .d = 7 };
+
+    bj::iostream_binaryizer<std::stringstream> iobin(std::ios::binary | std::ios::in | std::ios::out);
+    bj::ibinaryizer &ibin = iobin;
+    bj::obinaryizer &obin = iobin;
+
+    obin(thing1);
+
+    int version{}, a{}, b{}, c{}, d{};
+    ibin(version, a, b, c, d);
+    REQUIRE(version == 2);
+    REQUIRE(a == 1);
+    REQUIRE(b == 3);
+    REQUIRE(c == 5);
+    REQUIRE(d == 7);
+}
